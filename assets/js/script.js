f5e22790b5906a945efcdbdb1dcc33324f57c1e3
Majid Kargar <majidkargar01@gmail.com>
2023-02-03 01:38:16 +0330

    function ChangeDisplay va deleteInputValue ezafe shod

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// Variable ha
+
+// addad ha
+let numbersEl = document.querySelectorAll('.numbers'),
+    inputEL = document.querySelector('#answer-input'),
+    deleteAllBtn = document.querySelector('#delete-all')
+
+
+
+
+// event ha
+deleteAllBtn.addEventListener("click", deleteInputValue )
+
+
+// function ha
+
+// TITLE: ChangeDisplay Function
+// har dokme numbers ke click beshe , meqdaresh vared input mishe + harchi ke az qabl bode
+function ChangeDisplay() {
+    // numbers
+    numbersEl.forEach(item => {
+        item.addEventListener('click', () => {
+            inputEL.value += item.textContent   
+        })
+    })
+
+}
+
+// TITLE: delete input text
+// value input ro khali mikone
+function deleteInputValue() {
+    inputEL.value = ''
+}
+
+// TITLE: load function ha bad az load safhe
+document.addEventListener('DOMContentLoaded', () => {
+    ChangeDisplay()
+})

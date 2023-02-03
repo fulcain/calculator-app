4aa985e65f81e15b7af6ca003f35692c71ecea45
Hamed <hamedpyn@gmail.com>
2023-02-03 13:46:50 +0330

    Factorial

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,3 +36,13 @@ function deleteInputValue() {
 document.addEventListener('DOMContentLoaded', () => {
     ChangeDisplay()
 })
+
+
+// TITLE: factorial
+function fact(n) {
+    if (n == 0) {
+        return 1
+    } else {
+        return n * fact(n - 1)
+    }
+}
